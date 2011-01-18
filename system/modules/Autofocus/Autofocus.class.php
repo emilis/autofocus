3f2c740a6034cb20b8f11e3eaf7c97e245f861d6
Emilis Dambauskas <emilis.d@gmail.com>
2011-01-18 15:50:21 +0200

    Cut out design in static HTML file into templates.

--- system/modules/Autofocus/Autofocus.class.php
+++ system/modules/Autofocus/Autofocus.class.php
@@ -1,8 +1,9 @@
 <?php
 
+require_once LIB_DIR . "/ctlCommonApps/ctlCommonSiteApp.class.php";
 
-class Autofocus {
+class Autofocus extends ctlCommonSiteApp {
     function showIndex() {
-        return 'Autofocus.showIndex';
+        return $this->showContent("showIndex");
     }
 }
--- system/modules/Autofocus/AutofocusSite.class.php
+++ system/modules/Autofocus/AutofocusSite.class.php
@@ -5,6 +5,6 @@ require_once LIB_DIR . "/ctlSmi/ctlSite.class.php";
 class AutofocusSite extends ctlSite {
 
     function showIndex() {
-        return $this->showContent( file_get_contents(WEB_DIR . '/list.html') );
+        return loadObject("Autofocus")->showIndex();
     }
 }
